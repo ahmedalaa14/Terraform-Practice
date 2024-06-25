d77c9696980da0f7f6579f3e4f5ff663caff26f9
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-06-25 19:59:50 +0300

    outputs.tf done

---
+++ outputs.tf
@@ -0,0 +1,20 @@
+#account login outputs
+
+output "accountID" {
+
+  value =module.iam.accountID
+  
+}
+output "username" {
+  value=module.iam.user_name
+}
+
+output "passs" {
+
+  value = module.iam.usernamepass
+  
+}
+
+output "aws_autoscaling_group" {
+  value = module.asg.aws_autoscaling_group  
+}
