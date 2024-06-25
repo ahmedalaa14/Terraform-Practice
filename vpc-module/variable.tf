58a58070dc3ed6bff48321f1a57f4e4a35fd8f11
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-06-25 18:36:47 +0300

    create variables.tf

---
+++ vpc-module/variable.tf
@@ -0,0 +1,35 @@
+variable "vpc-cider" {
+    description = "CIDR block for the VPC"
+    type        = string
+    default     = "10.16.0.0/16"
+}
+#======================================================#
+
+variable "public_subnets_cidr_blocks" {
+    description = "CIDR blocks for public subnets"
+    type        = list(string)
+    default     = [ "10.16.0.0/19","10.16.32.0/19","10.16.64.0/19",
+                    "10.16.96.0/19","10.16.128.0/19","10.16.160.0/19"]
+}
+#=======================================================#
+
+variable "map_public_ip_on_launch" {
+    description = "define if a public IP is assigned to instances launched in the subnet"
+    type        = bool
+    default     = true
+}
+
+
+#tags
+
+variable "tags" {
+  description = "Tags to apply to ASG resources"
+  type        = map(string)
+  default     = {
+        company="fawry"
+        team   ="devops"
+  }
+}
+
+
+#=======================================================#
